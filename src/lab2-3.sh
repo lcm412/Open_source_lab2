b6bc4524b76ed44be24f3a305f0e968f164aa26e
lcm412 <91687581+lcm412@users.noreply.github.com>
2021-11-30 08:43:10 +0900

    Update lab2-3.sh

--- src/lab2-3.sh
+++ src/lab2-3.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 read weight height
+height=`echo "scale=2; $height/100"|bc` 
 
-BMI=`expr $weight / \( $height \* $height \)`
+BMI=`echo "scale=2; $weight/($height*$height)"|bc` 
 
-if [ ${BMI} -lt 18.5 ]; then
+if [ `echo "$BMI < 18.5"|bc` -eq 1 ]; then
 	echo "Underweight"
-elif [ ${BMI} -ge 18.5 ] && [ $BMI -lt 23 ]; then
+elif [ `echo "18.5 <= $BMI"|bc` -eq 1 ] && [ `echo "$BMI < 23"|bc` -eq 1 ]; then
 	echo "Normal weight"
 else
 	echo "Overweight"
