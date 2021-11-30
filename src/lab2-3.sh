68eed8eeb3c4103d47703e55e6a855377d5513c5
lcm412 <91687581+lcm412@users.noreply.github.com>
2021-11-29 16:32:41 -0800

    Update lab2-3.sh

--- src/lab2-3.sh
+++ src/lab2-3.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-read weight height
-height=`echo "scale=2; $height/100"|bc` 
+echo "type name that you want to search"
+read Searchname 
 
-BMI=`echo "scale=2; $weight/($height*$height)"|bc` 
+while read name phone
+do
+	if [ "$Searchname" = "$name" ]; then
+		echo "$name $phone"
+	fi
+done < DB.txt
 
-if [ `echo "$BMI < 18.5"|bc` -eq 1 ]; then
-	echo "Underweight"
-elif [ `echo "18.5 <= $BMI"|bc` -eq 1 ] && [ `echo "$BMI < 23"|bc` -eq 1 ]; then
-	echo "Normal weight"
-else
-	echo "Overweight"
-fi
 exit 0
